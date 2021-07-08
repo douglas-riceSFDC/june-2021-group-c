ad7f1a20e1446951bf71731efe8ce3985b70ccf1
Nate Shetler <nshetler@salesforce.com>
2021-07-08 09:17:54 -0400

    CLENHBC-603 add properly functioning reviewList and supporting components

--- force-app/main/default/lwc/reviewList/reviewList.js
+++ force-app/main/default/lwc/reviewList/reviewList.js
@@ -1,3 +1,5 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
 
-export default class ReviewList extends LightningElement {}
+export default class ReviewList extends LightningElement {
+    @api movieReviews;
+}
