cab74a49374ecaaa55d8025ea745ed7807b0b3ed
Nate Shetler <nshetler@salesforce.com>
2021-07-09 12:16:09 -0400

    CLENHBC-609 add fully functional average rating component

--- force-app/main/default/lwc/averageRating/averageRating.js
+++ force-app/main/default/lwc/averageRating/averageRating.js
@@ -6,6 +6,7 @@ export default class AverageRating extends LightningElement {
     @api recordId;
     @track stars = [];
     @track blankStars = [];
+    @track halfStar = [];
     @track averageRating;
 
     connectedCallback() {
@@ -15,17 +16,18 @@ export default class AverageRating extends LightningElement {
 
                 let totalStars = 5;
                 this.averageRating = this.title[0].Sum_Review_Ratings__c / this.title[0].Total_Reviews__c;
-
-                for (let i = 0; i < this.averageRating; i++) {
+                
+                for (let i = 0; i < Math.floor(this.averageRating / 1); i++) {
                     this.stars.push({Id: i});
                 }
                 
-                for (let i = 0; i < totalStars - this.averageRating; i++) {
+                for (let i = 0; i < Math.floor((totalStars - this.averageRating) / 1); i++) {
                     this.blankStars.push({Id: i});
                 }
 
-                console.log("Title", this.title);
-                console.log("Average Review:", this.averageRating);
+                if (this.averageRating % 1 !== 0) {
+                    this.halfStar.push({Id: 0});
+                }
             })
             .catch(error => {
                 console.error('Error occured'. error);
