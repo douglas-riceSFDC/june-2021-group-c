5bcbf8a5b6e1dfcde9ce5fe0e2d0589c66b20275
Nate Shetler <nshetler@salesforce.com>
2021-07-12 10:27:23 -0400

    CLENHBC-609 add error message changes and refactor star calculation

--- force-app/main/default/lwc/averageRating/averageRating.js
+++ force-app/main/default/lwc/averageRating/averageRating.js
@@ -6,31 +6,31 @@ export default class AverageRating extends LightningElement {
     @api recordId;
     @track stars = [];
     @track blankStars = [];
-    @track halfStar = [];
+    @track halfStar = false;
     @track averageRating;
+    @track totalStars = 5;
 
     connectedCallback() {
         getMovieRatingsInfo({recordId: this.recordId})
             .then(result => {
                 this.title = result;
 
-                let totalStars = 5;
                 this.averageRating = this.title[0].Sum_Review_Ratings__c / this.title[0].Total_Reviews__c;
                 
                 for (let i = 0; i < Math.floor(this.averageRating / 1); i++) {
                     this.stars.push({Id: i});
                 }
                 
-                for (let i = 0; i < Math.floor((totalStars - this.averageRating) / 1); i++) {
+                for (let i = 0; i < Math.floor((this.totalStars - this.averageRating) / 1); i++) {
                     this.blankStars.push({Id: i});
                 }
 
-                if (this.averageRating % 1 !== 0) {
-                    this.halfStar.push({Id: 0});
+                if (this.averageRating % 1 !== 0 && (this.stars.length + this.blankStars.length) !== 0) {
+                    this.halfStar = true;
                 }
             })
             .catch(error => {
-                console.error('Error occured'. error);
+                console.error('Error occured', error);
             });
     }
 }
