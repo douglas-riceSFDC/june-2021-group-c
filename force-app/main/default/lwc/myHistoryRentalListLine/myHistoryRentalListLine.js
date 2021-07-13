f99bff443e52ca78a6f5abcc71433fa6d72096d5
Scott Grisso <sgrisso@salesforce.com>
2021-07-13 10:20:24 -0400

    Add Movie Rentals List to my history

---
+++ force-app/main/default/lwc/myHistoryRentalListLine/myHistoryRentalListLine.js
@@ -0,0 +1,29 @@
+import { LightningElement, api, track } from 'lwc';
+import { NavigationMixin } from 'lightning/navigation';
+import getTitleFromRental from '@salesforce/apex/recommendationListAuraService.getTitleFromRental';
+
+export default class MyHistoryRentalListLine extends NavigationMixin(LightningElement) {
+    @api movieRental;
+    @track movie;
+
+    connectedCallback() {
+        getTitleFromRental({rental: this.movieRental})
+            .then(result => {
+                this.movie = result;
+                console.log(this.title);
+            })
+            .catch(error => {
+                console.error('Error occurred', error);
+            });        
+    }
+
+    handleTitleSelection() {
+        this[NavigationMixin.Navigate]({
+            type: 'standard__recordPage',
+            attributes: {
+				recordId: this.movie.Id,
+                actionName: 'view',
+            }
+        });
+    }
+}
